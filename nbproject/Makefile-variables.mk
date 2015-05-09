77352d652f9978a90651056d5f9683e8edb14bcc
Nicolò Vescera <slayercandy@icloud.com>
2015-05-09 09:51:36 +0200

    Update main.c

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
 CND_ARTIFACT_NAME_Debug=ese037-elencostudentileggicsv
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/ese037-elencostudentileggicsv
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/ese037-elencostudentileggicsv
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Debug=ese037-elencostudentileggicsv.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/ese037-elencostudentileggicsv.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/ese037-elencostudentileggicsv.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
